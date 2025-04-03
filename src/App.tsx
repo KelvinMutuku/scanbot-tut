47c88d290321ec2a1ce28a37ca61ef15e8ecd469
Kelvin Mutuku <mutukuk553@gmail.com>
2025-04-03 18:12:39 +0300

    Updated App.tsx and created wasm folder in public directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
-import ScanbotSDK from 'scanbot-web-sdk/ui';
+import { useEffect, useState } from "react";
+import ScanbotSDK from "scanbot-web-sdk/ui";
 
 const App = () => {
+  const [scanResult, setScanResult] = useState<string>("");
+  
   useEffect(() => {
     const init = async () => {
       await ScanbotSDK.initialize({
@@ -12,8 +14,25 @@ const App = () => {
 
     init();
   }, []);
+  
+  const startScanner = async () => {
+    const config = new ScanbotSDK.UI.Config.BarcodeScannerScreenConfiguration();
+          
+    const result = await ScanbotSDK.UI.createBarcodeScanner(config);
+        
+    if (result && result.items.length > 0) {
+      setScanResult(result.items[0].barcode.text);
+    }
+    
+    return result;
+  }
 
-  return <></>;
-}
+  return (
+    <div>
+      <button onClick={startScanner}>Start Scanner</button>
+      {scanResult && <div>{scanResult}</div>}
+    </div>
+  );
+};
 
 export default App;
